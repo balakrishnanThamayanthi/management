1424e763cd5978c29a4a55bdebc30a6d0012efa4
Balakrishnan Thamayanthi <manothamayanthi98@gmail.com>
2023-02-03 15:18:48 +0530

    V1U1

---
+++ src/App.js
@@ -0,0 +1,49 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
+import { BrowserRouter, Route, NavLink, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     <div className="App container">
+      <h3 className='d-flex justify-content-conter m-3'>Hospital Management System</h3>
+
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <u1 className="navbar-nav">
+          <li className="nav-it em- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/department">
+              Department
+            </NavLink>
+          </li> 
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+              Employee
+            </NavLink>
+          </li>         
+        </u1>
+      </nav>
+
+      
+
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/department' element={<Department/>}/>
+        <Route path='/employee' element={<Employee/>}/>
+      </Routes>
+      
+     </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
